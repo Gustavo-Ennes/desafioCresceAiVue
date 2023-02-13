4e33bd7c5eabdb72cb75cd6c5de9021929e32b81
Gustavo Ennes <gustavo@ennes.dev>
2023-02-12 23:02:24 -0300

    Add router, bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,9 @@
-<script setup lang="ts">
-// import HelloWorld from "./components/HelloWorld.vue";
-// import TheWelcome from "./components/TheWelcome.vue";
+<script lang="ts">
+export default {
+  name: "app",
+};
 </script>
 
-<script lang="ts"></script>
-
 <template>
-  <h1>Meu <span>hello</span> world</h1>
+  <router-view />
 </template>
-
-<style scoped lang="scss">
-h1 {
-  span {
-    color: red;
-  }
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
 import { createApp } from "vue";
-import App from "./App.vue";
+import AppVue from "./App.vue";
 
-createApp(App).mount("#app");
+import "./assets/app.scss";
+import { router } from "./router/router";
+
+// 5. Create and mount the root instance.
+const app = createApp(AppVue);
+// Make sure to _use_ the router instance to make the
+// whole app router-aware.
+app.use(router);
+
+app.mount("#app");
